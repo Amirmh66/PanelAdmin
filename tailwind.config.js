21f309b61fee3e006417c992637903f47d98d4b2
amirmh66 <looperxz67@gmail.com>
2024-08-25 15:48:50 +0330

    Fix Theme Section with alpine

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
+  darkMode: 'class',
   theme: {
-    extend: {
-
-      animation: ['hover'],
-      boxShadow: {
-        glass: 'inset 0px 0px 0px 0px rgba(255,255,255, 0.6)',
-      }
-    },
+    extend: {},
   },
-  plugins: [
-
-  ],
+  plugins: [],
 }
 
